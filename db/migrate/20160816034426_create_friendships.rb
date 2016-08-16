68827a79764240e681160321048d9546bff0f300
Naveen <nk1383886@gmail.com>
2016-08-16 10:08:31 +0530

    all code

---
+++ db/migrate/20160816034426_create_friendships.rb
@@ -0,0 +1,9 @@
+class CreateFriendships < ActiveRecord::Migration[5.0]
+  def change
+    create_table :friendships do |t|
+    	t.integer :friend_id
+    	t.integer :user_id
+      t.timestamps
+    end
+  end
+end
